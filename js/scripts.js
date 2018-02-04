b431a0d1b4c503e62d0dc34e9ad2b8cd18bff708
Wojtek Gumulski <wojciech.gumulski@gmail.com>
2018-02-04 21:35:31 +0100

    Correct scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var classButton = document.getElementsByClassName('button');
 
 console.log(classButton);
 
-for (i = 0 ; i < classButton.length ; i++) {
+for (var i = 0 ; i < classButton.length ; i++) {
 
 	alert(classButton[i].innerText);
 }
